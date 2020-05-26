630fcf9d7f331c717fea5bd2afb107e465780c93
lubos klokner <lubos@klokner.sk>
2020-05-26 13:58:10 +0200

    F5-CLI/scripts tunning

--- F5-CLI/03_do.sh
+++ F5-CLI/03_do.sh
@@ -8,11 +8,15 @@ f5 config set-defaults --disable-ssl-warnings true --auto-approve
 echo "Login to BIG-IP..."
 f5 login --authentication-provider bigip --host ${BIGIP_MGMT} --user ${BIGIP_ADMIN} --password ${BIGIP_ADMIN_PWD}
 
+echo
 echo "Install DO Extension..."
 f5 bigip extension do install
 f5 bigip extension do verify
+sleep 5
 
+echo
 echo "Deploy DO JSON..."
 f5 bigip extension do create --declaration ${DO_JSON}
+echo
 echo "Verify the deployed configuration..."
 f5 bigip extension do show
--- F5-CLI/04_as3.sh
+++ F5-CLI/04_as3.sh
@@ -8,13 +8,16 @@ f5 config set-defaults --disable-ssl-warnings true --auto-approve
 echo "Login to BIG-IP..."
 f5 login --authentication-provider bigip --host ${BIGIP_MGMT} --user ${BIGIP_ADMIN} --password ${BIGIP_ADMIN_PWD}
 
+echo
 echo "Install AS3 Extension..."
 f5 bigip extension as3 install
 f5 bigip extension as3 verify
+sleep 5
 
+echo
 echo "Deploy AS3 JSONs..."
 f5 bigip extension as3 create --declaration ${AS3_JSON_DVWA}
 f5 bigip extension as3 create --declaration ${AS3_JSON_FWD}
-
+echo
 echo "Verify the deployed configuration..."
 f5 bigip extension as3 show
