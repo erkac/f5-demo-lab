4b7a59ee844520638aa8ed6215778342e31ca25c
lubos klokner <lubos@klokner.sk>
2023-03-31 13:13:44 +0000

    tuned to UDF where DHCP is used

--- F5-CLI/03_system_tuning.sh
+++ F5-CLI/03_system_tuning.sh
@@ -3,8 +3,8 @@
 . ./00_environment.sh
 
 echo "Tuning BIG-IP System for Automation Toolchain deployment..."
-echo "MGMT DHCP Disabled"
-ssh -q ${BIGIP_ROOT}@${BIGIP_MGMT} "tmsh modify sys global-settings mgmt-dhcp disabled"
+#echo "MGMT DHCP Disabled"
+#ssh -q ${BIGIP_ROOT}@${BIGIP_MGMT} "tmsh modify sys global-settings mgmt-dhcp disabled"
 echo "Set Admin Password"
 ssh -q ${BIGIP_ROOT}@${BIGIP_MGMT} "tmsh modify auth user admin password ${BIGIP_ADMIN_PWD}"
 echo "Set DNS Server to 1.1.1.1"
